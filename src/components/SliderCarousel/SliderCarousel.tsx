f4d59327343ac8f9b8addbe362237efb45ed21a0
Maks262626 <maxum2003la@gmail.com>
2023-08-25 20:39:36 +0300

    fix slider height

--- src/components/SliderCarousel/SliderCarousel.tsx
+++ src/components/SliderCarousel/SliderCarousel.tsx
@@ -51,6 +51,8 @@ const SliderCarousel = () => {
                     </div>
                     <div className="slide__text">
                         {slide.description}
+                        {slide.description}
+                        {slide.description}
                     </div>
                     <button className="slide__btn btn" onClick={()=>{handleClick(slide)}}>
                         <svg viewBox="0 0 22 20" xmlns="http://www.w3.org/2000/svg">
