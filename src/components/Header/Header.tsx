93b6a6ea9c8662a266891028630708e3a8aafbf5
Maks262626 <maxum2003la@gmail.com>
2023-08-25 14:33:23 +0300

    categories now show only in main page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,23 +2,25 @@ import s from './Header.module.scss';
 import logo from '../../assets/img/logo.png';
 import arrowRight from '../../assets/icon/arrowRight.svg';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { setActivePath } from '../../redux/menu/slice';
 import { selectFilters } from '../../redux/filters/selectors';
 import { selectMenu } from '../../redux/menu/selectors';
 import MenuItem from '../MenuItem/MenuItem';
 import CategoriesItem from '../CategoriesItem/CategoriesItem';
 import { useState } from 'react';
+import BurgerMenu from '../BurgerMenu/BurgerMenu';
 
 const Header:React.FC = () => {
     const {categories} = useSelector(selectFilters);
     const {data:menu} = useSelector(selectMenu);
     const [burgerActive,setBurgerActive] = useState<boolean>(false);
     const dispatch = useDispatch();
-
+    const location = useLocation();
     return (
         <>
             <header className={`${s.header} ${burgerActive && s.active}`} onClick={()=>{setBurgerActive(false)}}>
+                <BurgerMenu burgerActive={burgerActive} setBurgerActive={setBurgerActive}/>
                 <div className={s.content}>
                     <Link to="/" className={s.logo} onClick={()=>{dispatch(setActivePath("/"))}}>
                         <img src={logo} alt="logo" />
@@ -28,7 +30,7 @@ const Header:React.FC = () => {
                             return <MenuItem item={item} key={index}/>
                         })}
                     </ul>
-                    <div className={s.storeCategories}>
+                    <div className={`${s.storeCategories} ${location.pathname === '/' && s.show}`} >
                         <div className={s.title}>
                             <span>Category</span>
                             <img src={arrowRight} alt="arrowRight" />
@@ -42,11 +44,7 @@ const Header:React.FC = () => {
                 </div>
                 
             </header>
-            <div className={`${s.burger} ${burgerActive && s.active}`} onClick={()=>{setBurgerActive(!burgerActive)}}>
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
+            <BurgerMenu burgerActive={burgerActive} setBurgerActive={setBurgerActive}/>
         </>
     );
 }
